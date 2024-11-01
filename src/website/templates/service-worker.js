ebd3ee137b0fa5d6cd262a4ae79e53c9bdd5e069
Kliiyu <adri2007900@gmail.com>
2024-11-01 13:59:08 +0100

    added: flask app and docker compose

---
+++ src/website/templates/service-worker.js
@@ -0,0 +1,9 @@
+const cacheName = "Friday";  
+const assets = ["/"];  // Eventuelle ekstra sider som "kan fungere offline", ikke nødvendig
+self.addEventListener("install", (installEvent) => {  
+  installEvent.waitUntil(  
+    caches.open(cacheName).then((cache) => {  
+      cache.addAll(assets);  
+    })  
+  );  
+});
